5054f6b32c0c04c396ac3d43b6e4657bdec32c19
Alexander Tipantiza <latipantiza1@espe.edu.ec>
2026-04-22 22:46:34 -0500

    chickenfarm V1.0

---
+++ ws/tipantiza/u1/ws04ClassDiagram/ChickenFarm/ChickenFarm.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+package chickenfarm;
+
+/**
+ *
+ * @author Lenin Alexander Tipantiza Muñoz, Espe
+ */
+public class ChickenFarm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello chickens, this is Alexander Tipantiza's Farm");
+    }
+}
