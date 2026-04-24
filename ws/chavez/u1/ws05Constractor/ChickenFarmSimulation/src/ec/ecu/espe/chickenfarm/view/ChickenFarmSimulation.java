853e63e86007c11eba7cfb5c1a1d2fb3ecaf43d0
odachavez <oychavez@espe.edu.ec>
2026-04-23 23:49:10 -0500

    ChickenFarmSimulator v1.0

---
+++ ws/chavez/u1/ws05Constractor/ChickenFarmSimulation/src/ec/ecu/espe/chickenfarm/view/ChickenFarmSimulation.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ec.ecu.espe.chickenfarm.view;
+
+import ec.ecu.espe.chickenfarm.model.Chicken;
+
+/**
+ *
+ * @author Odalys Chavez, CodeBreakers, @ESPE
+ */
+public class ChickenFarmSimulation {
+     public static void main(String[] args) {
+        Chicken chicken;
+        chicken = new Chicken(1,"Lucy","White and brown",1,true);
+        System.out.println("My chicken is -->" +chicken);
+    }
+    
+}
