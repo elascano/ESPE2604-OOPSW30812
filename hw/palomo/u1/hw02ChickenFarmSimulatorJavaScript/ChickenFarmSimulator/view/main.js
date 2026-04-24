84d0110230778e95b28b1a244df27a73ae029128
Cristian-Palomo <cdpalomo@espe.edu.ec>
2026-04-23 20:55:05 -0500

    ChickenFarmSimulator in JavaSxript v1.0.0

---
+++ hw/palomo/u1/hw02ChickenFarmSimulatorJavaScript/ChickenFarmSimulator/view/main.js
@@ -0,0 +1,10 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
+ */
+const Chicken = require("./model/Chicken.js");
+
+const lucy = new Chicken(1, "Lucy", "Brown", 2, false);
+
+console.log(lucy.toString());
+
--- ws/palomo/u1/ws05Constructor/ChickenFarmSimulator/src/chickenfarmsimulator/ChickenFarmSimulator.java
+++ ws/palomo/u1/ws05Constructor/ChickenFarmSimulator/src/chickenfarmsimulator/ChickenFarmSimulator.java
@@ -2,17 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package ec.edu.espe.chickenfarm.view;
+package chickenfarmsimulator;
 
-import ec.edu.espe.chickenfarm.model.Chicken;
 /**
  *
- * @author Cristian, Erro 404, @ESPE
+ * @author Cristian
  */
 public class ChickenFarmSimulator {
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        Chicken chicken;
-        chicken = new Chicken(1,"Lucy","White and brown",1,true);
-        System.out.println("my chicken is -->" + chicken);
+        // TODO code application logic here
     }
+    
 }
