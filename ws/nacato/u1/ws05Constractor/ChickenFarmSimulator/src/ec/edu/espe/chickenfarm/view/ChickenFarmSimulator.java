228dfd05ffc75ae2e9eecc35219c538466ee8828
angienx <angie.nacato3a@gmail.com>
2026-04-24 20:04:03 -0500

    ws05ConstractorV1.0

---
+++ ws/nacato/u1/ws05Constractor/ChickenFarmSimulator/src/ec/edu/espe/chickenfarm/view/ChickenFarmSimulator.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.chickenfarm.view;
+
+/**
+ *
+ * @author Angie Nacato, Error 404, @ESPE
+ */
+public class ChickenFarmSimulator {
+    
+    public static void main (String []args){
+    Chicken chicken;
+       
+    chicken = new Chicken (1, "Lucy","White and Brown",1,true);
+    System.out.println("My Chicken is ---->"+chicken);
+       
+   }    
+}
