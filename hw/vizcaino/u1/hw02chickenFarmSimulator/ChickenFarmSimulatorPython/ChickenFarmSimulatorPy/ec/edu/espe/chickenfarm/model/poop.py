9175b1e231973ab0bdca88397e398859a47bc5d2
adrian gustavo vizcaino saenz <agvizcaino@espe.edu.ec>
2026-04-25 01:42:46 -0500

    t i remove broken python structure
    hm02chickenfarmsimulatorfailed #	deleted:    hw02chickenFarmSimulator/ChickenFarmSimulatorPython/ChickenFarmSimulatorPy/ec/edu/espe/chickenfarm/model/poop.py

--- hw/vizcaino/u1/hw02chickenFarmSimulator/ChickenFarmSimulatorPython/ChickenFarmSimulatorPy/ec/edu/espe/chickenfarm/model/poop.py
+++
@@ -1,7 +0,0 @@
-class Poop:
-    def __init__(self, size, color):
-        self.size = size
-        self.color = color
-
-    def __str__(self):
-        return f"Poop{{size={self.size}, color={self.color}}}"
