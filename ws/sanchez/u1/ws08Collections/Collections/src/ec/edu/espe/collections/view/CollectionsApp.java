c67829b9838afff480c171d0fd9c97f9c8da5f79
AndresSl-D-04 <jasanchez59@espe.edu.ec>
2026-05-06 21:18:41 -0500

    Collections in Java, JavaScript and Python

---
+++ ws/sanchez/u1/ws08Collections/Collections/src/ec/edu/espe/collections/view/CollectionsApp.java
@@ -0,0 +1,79 @@
+package ec.edu.espe.collections.view;
+
+import ec.edu.espe.collections.model.Person;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ *
+ * @author Joel Sanchez, The Softwarriors, @ESPE
+ */
+public class CollectionsApp {
+    public static void main(String[] args) {
+        int id;
+        String fullName;
+        LocalDate bornOnDate;
+        boolean alive;
+        
+        id = 1;
+        fullName = "Sanchez Joel";
+        bornOnDate = LocalDate.of(2006, 07, 04);
+        alive = true;
+        
+        Person person = new Person(id, fullName, bornOnDate, alive);
+        
+        System.out.println(person);
+        System.out.println("Age of person 1 is " + person.computeAgeInYears());
+        
+        id = 2;
+        fullName = "Veronica Lanchimba";
+        bornOnDate = LocalDate.of(1982, 01, 04);
+        alive = false;
+        
+        Person person2 = new Person(id, fullName, bornOnDate, alive);
+        
+        System.out.println("Age of person 2 is " + person.computeAgeInYears());
+        
+        Collection things;
+        things = new ArrayList<>();
+        System.out.println("size of things -->" + things);
+        
+        things.add(8000);
+        things.add(3.5F);
+        things.add("Quito");
+        things.add(true);
+        
+        printTheCollection(things);
+        
+        things.add(person);
+        things.add(person2);
+        
+        printTheCollection(things);
+        
+        things.add(4078.76F);
+        things.add("a");
+        
+        printTheCollection(things);
+        
+        ArrayList<Person> people = new ArrayList<>();
+        people.add(person);
+        people.add(person2);
+        
+        System.out.println("\npeople --> " + people);
+        System.out.println("");
+        
+        people.forEach((p)->{
+        System.out.println("Person --> " + p);
+    });
+        
+        
+    }
+    
+    private static void printTheCollection(Collection things){
+        System.out.println("\nsize of things --> " + things.size());
+        System.out.println("These are my things --> \n" + things);
+    }
+
+    
+}
