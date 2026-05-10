9ad1c17e6483fd95ef458367d80d6690e145dbd9
Didier Robin Elbay Enriquez <zeus.didier2006@gmail.com>
2026-05-10 17:11:19 -0500

    Update Chiken.java

--- ws/elbay/u1/w04ClassCostructor/src/main/java/ec/edu/espe/chikencoops/model/Chiken.java
+++ ws/elbay/u1/w04ClassCostructor/src/main/java/ec/edu/espe/chikencoops/model/Chiken.java
@@ -14,7 +14,7 @@ public class Chiken {
     private int age;
 
     public Chiken(int id, String color, Date bornOnDate, boolean molting, int age) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        throw new UnsupportedOperationException("Not supported yet."); 
     }
 
     public int getId() {
