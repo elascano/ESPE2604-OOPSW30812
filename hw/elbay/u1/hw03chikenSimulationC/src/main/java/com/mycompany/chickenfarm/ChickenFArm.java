c64e711d344abad9f0f039f9aa219967f4bd2b66
Didier Robin Elbay Enriquez <zeus.didier2006@gmail.com>
2026-05-14 00:14:18 -0500

    Add files via upload

---
+++ hw/elbay/u1/hw03chikenSimulationC/src/main/java/com/mycompany/chickenfarm/ChickenFArm.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.chickenfarm;
+public class ChickenFArm {
+
+    public static void main(String[] args) {
+        System.out.println("Hello chikens,this is Didier Elbay ");
+    }
+}
