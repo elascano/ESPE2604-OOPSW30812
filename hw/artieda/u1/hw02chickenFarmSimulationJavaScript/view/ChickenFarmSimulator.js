c3dc27bc80cfffb24a3252246e66c9a7d40a80dd
Ronald Tipan <rdtipan2@espe.edu.ec>
2026-05-13 20:59:09 -0500

    ChickenFarmSimulator in JavaScript and Python

--- hw/artieda/u1/hw02chickenFarmSimulationJavaScript/view/ChickenFarmSimulator.js
+++
@@ -1,8 +0,0 @@
-import { Chicken } from "../model/Chicken.js";
-
-export class ChickenFarmSimulator {
-    mostrar() {
-        this.chicken = new Chicken(1, "Lucy", "White and brown", 1, true);
-        console.log("my chicken is -->" + this.chicken.toString());
-    }
-}
