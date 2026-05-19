5195bafc9f329ea3cdbfaf080c6729174da354d3
NASE SOLORZANO JENNYFER NICOLE <jnnase@espe.edu.ec>
2026-05-18 21:53:34 -0500

    Workshop 1 Unit 2

--- hw/nase/u1/hw03chickenFarmSimulador/main.js
+++
@@ -1,4 +0,0 @@
-// Author Jennyfer Nase
-const Chicken = require('./JS_chicken/model/chicken'); 
-const lucy = new Chicken(1, "Lucy", "White and Brown", 1, true); 
-console.log("My chicken -->", lucy.toString());
---
+++ ws/nase/u2/Storeproducts/src/main/java/ec/edu/espe/Storeproducts/model/Product.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.Storeproducts.model;
+
+/**
+ *
+ * @author LABS-ESPE
+ */
+    public class Product {
+
+    private String name;
+    private int quantity;
+
+    public Product(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
