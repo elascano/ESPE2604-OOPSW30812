9297603a149007a659562bc405b00301ca92a18b
Brandon Steeven Collahuazo Cumbajin <brandon1stev2@gmail.com>
2026-05-20 20:47:15 -0500

    ws15GradesStudentAppGUI

---
+++ ws/collahuazo/u2/ws15GradesStudentAppGUI/GradesStudentAppGUI/src/ec/edu/espe/view/MainLauncher.java
@@ -0,0 +1,8 @@
+package ec.edu.espe.view;
+
+public class MainLauncher {
+    public static void main(String[] args) {
+        // Llama al launch de JavaFX desde una clase ordinaria
+        GradesStudentApp.main(args);
+    }
+}
