42503f47e741863c21304eabc6150612c65e7053
Artieda <mateoartieda.2201@gmail.com>
2026-05-21 13:35:47 -0500

    Libraries

---
+++ ws/artieda/u2/ws16/StoreApp/StoreApp/src/ec/edu/espe/store/model/Product.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.store.model;
+
+/**
+ *
+ * @author Mateo Artieda,MKA Programer, @ESPE
+ */
+public class Product {
+    private int id;
+    private String description;
+    private float price;
+    private float pvp;
+
+    public Product(int id, String description, float price) {
+        this.id = id;
+        this.description = description;
+        this.price = price;
+        
+        //TODO cumpute totaL price
+        pvp = Tax.computeTotal(price,15F);
+    }
+
+    public Product(int id, String description, float price, float pvp) {
+        this.id = id;
+        this.description = description;
+        this.price = price;
+        this.pvp = pvp;
+    }
+
+    @Override
+    public String toString() {
+        return "Product\n{" + "id=" + id + ", description=" + description + ", price=" + price + ", pvp=" + pvp + '}';
+    }
+    
+    
+    
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the price
+     */
+    public float getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    /**
+     * @return the pvp
+     */
+    public float getPvp() {
+        return pvp;
+    }
+
+    /**
+     * @param pvp the pvp to set
+     */
+    public void setPvp(float pvp) {
+        this.pvp = pvp;
+    }
+    
+    
+}
