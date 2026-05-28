988ae9407dcddb57c74a4951fe8d3568226682f3
Alexander Tipantiza <latipantiza1@espe.edu.ec>
2026-05-27 23:51:59 -0500

    ws11 TestUnit in Python and JavaScript V1.0

---
+++ hw/tipantiza/u2/hw11UnitTest/JavaScript/calculator/src/calculator/main.js
@@ -0,0 +1,9 @@
+const BasicOperation = require('./controller/basic_operation');
+
+function main() {
+    console.log("Test Basic Operation, from ALexander Tipantiza!");
+}
+
+if (require.main === module) {
+    main();
+}
