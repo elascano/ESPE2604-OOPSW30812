88d67ef7a5d4e9cfe5fee11b8f6cb6901b4236d7
Lomas Haro Christopher Josue <cjlomas@espe.edu.ec>
2026-06-05 21:15:30 -0500

    Update billingController.js

--- hw/lomas/u2/hw14DBMongoandGUI/hw14DBMongoandGUI.js/controller/billingController.js
+++ hw/lomas/u2/hw14DBMongoandGUI/hw14DBMongoandGUI.js/controller/billingController.js
@@ -20,7 +20,7 @@ class BillingController {
         }
     }
 
-    // --- PERSON METHODS ---
+    
     async saveOrUpdatePerson(personObj) {
         if (!this.db) throw new Error("No database connection.");
         const collection = this.db.collection('persons');
@@ -32,7 +32,7 @@ class BillingController {
         return await this.db.collection('persons').findOne({ _id: personId });
     }
 
-    // --- PRODUCT METHODS ---
+  
     async saveOrUpdateProduct(productObj) {
         if (!this.db) throw new Error("No database connection.");
         await this.db.collection('products').updateOne({ _id: productObj.productId }, { $set: productObj.toDict() }, { upsert: true });
@@ -43,7 +43,7 @@ class BillingController {
         return await this.db.collection('products').findOne({ _id: productId });
     }
 
-    // --- PAYMENT METHODS ---
+ 
     async saveOrUpdatePayment(paymentObj) {
         if (!this.db) throw new Error("No database connection.");
         await this.db.collection('payments').updateOne({ _id: paymentObj.paymentId }, { $set: paymentObj.toDict() }, { upsert: true });
