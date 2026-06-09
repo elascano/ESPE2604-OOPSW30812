02a24259c38da72570fc14a76bb3a375c6c36a71
Didier Robin Elbay Enriquez <zeus.didier2006@gmail.com>
2026-06-08 20:55:31 -0500

    Delete hw/elbay directory

--- hw/elbay/hw11UnitTest/BasicOperation.py
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        float sum = BasicOperation.add(5, 3);
-        float difference = BasicOperation.subtract(5, 3);
-
-        System.out.println("Suma: " + sum);
-        System.out.println("Resta: " + difference);
-    }
-}
