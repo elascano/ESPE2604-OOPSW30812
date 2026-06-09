769d73449530f0bbfc8ad401c784c09d61217cd6
Cristian-Palomo <cdpalomo@espe.edu.ec>
2026-06-08 21:15:45 -0500

    LoginandSplash

---
+++ ws/palomo/u2/ws18GUISwing/ec.espe.edu_shoppingCenter_jar_1.0-SNAPSHOT/src/main/java/ec/espe/edu/shoppingcenter/ShoppingCenter.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.espe.edu.shoppingcenter;
+
+/**
+ *
+ * @author Cristian
+ */
+public class ShoppingCenter {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
