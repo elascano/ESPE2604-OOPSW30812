0371c128aa781cb1bec8c4972bbe51d20fea5de2
Didier Robin Elbay Enriquez <zeus.didier2006@gmail.com>
2026-06-08 21:04:59 -0500

    Add files via upload

---
+++ hw/elbay/u2/hw11UnitTest/BasicOperation.py
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        float sum = BasicOperation.add(5, 3);
+        float difference = BasicOperation.subtract(5, 3);
+
+        System.out.println("Suma: " + sum);
+        System.out.println("Resta: " + difference);
+    }
+}
