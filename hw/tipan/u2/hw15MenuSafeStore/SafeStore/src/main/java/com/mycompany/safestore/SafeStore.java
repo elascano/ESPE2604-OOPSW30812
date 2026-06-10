034ecbcc93b68679bad2b98d68f9e95dc9322f63
Ronald Tipan <rdtipan2@espe.edu.ec>
2026-06-09 22:21:16 -0500

    Homework MenuSafeStore

---
+++ hw/tipan/u2/hw15MenuSafeStore/SafeStore/src/main/java/com/mycompany/safestore/SafeStore.java
@@ -0,0 +1,13 @@
+package com.mycompany.safestore;
+
+import ec.edu.espe.safestore.view.FrmSupplier;
+
+public class SafeStore {
+
+    public static void main(String[] args) {
+
+        FrmSupplier frmSupplier = new FrmSupplier();
+        frmSupplier.setVisible(true);
+
+    }
+}
---
+++ hw/tipan/u2/hw15MenuSafeStore/SafeStore/src/main/java/ec/edu/espe/safestore/model/Supplier.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.safestore.model;
+
+/**
+ *
+ * @author ronal
+ */
+public class Supplier {
+     private int id;
+    private String name;
+    private String contactPerson;
+    private String phone;
+    private String email;
+    private String address;
+    private double creditTerm;
+    private double creditLimit;
+    
+}
+
