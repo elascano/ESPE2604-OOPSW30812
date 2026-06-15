49424754ecfb83411637c9b2bc967ed75354b437
ESTEBAN ALEXANDER BASURTO FARIAS <eabasurto@espe.edu.ec>
2026-06-15 11:00:37 -0500

    upload exam practical

---
+++ exams/basurto/u2/SoccerPlayers/SoccerPlayers/src/main/java/com/mycompany/soccerplayers/SoccerPlayers.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.soccerplayers;
+
+/**
+ *
+ * @author LABS-ESPE
+ */
+public class SoccerPlayers {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
