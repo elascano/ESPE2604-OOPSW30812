dc953c5beda8463349811964cbb3d36290d0af49
ESTEBAN ALEXANDER BASURTO FARIAS <eabasurto@espe.edu.ec>
2026-06-15 10:11:30 -0500

    Upoad exam2

---
+++ exams/basurto/u2/EXAM2/Exam2/src/main/java/com/mycompany/exam2/Exam2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exam2;
+
+/**
+ *
+ * @author LABS-ESPE
+ */
+public class Exam2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
