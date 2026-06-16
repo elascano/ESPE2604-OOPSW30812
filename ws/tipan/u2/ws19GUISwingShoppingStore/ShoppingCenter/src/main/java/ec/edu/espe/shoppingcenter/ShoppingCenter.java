f6e5b7a3bc2f81f8ebc3d931e6ad5df181dea028
Ronald Tipan <rdtipan2@espe.edu.ec>
2026-06-15 23:25:16 -0500

    Add ws19GUISwing V1.0

---
+++ ws/tipan/u2/ws19GUISwingShoppingStore/ShoppingCenter/src/main/java/ec/edu/espe/shoppingcenter/ShoppingCenter.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espe.shoppingcenter;
+
+/**
+ *
+ * @author Ronald Tipan <The_Softwarrios at ESPE>
+ */
+public class ShoppingCenter {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
