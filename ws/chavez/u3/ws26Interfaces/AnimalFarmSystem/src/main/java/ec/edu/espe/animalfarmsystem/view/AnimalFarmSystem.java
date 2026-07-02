34b57bc43a30ee2c68b65413ddc0cb6343660e2a
odachavez <oychavez@espe.edu.ec>
2026-07-01 19:54:45 -0500

    ws26

---
+++ ws/chavez/u3/ws26Interfaces/AnimalFarmSystem/src/main/java/ec/edu/espe/animalfarmsystem/view/AnimalFarmSystem.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espe.animalfarmsystem.view;
+
+import ec.edu.espe.animalfarmsystem.model.Pig;
+import java.util.Date;
+
+/**
+ *
+ * @author Odalys Chavez , CodeBreakers, @ESPE
+ */
+public class AnimalFarmSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Pig pig = new Pig(0, 0, "Cerdo", new Date(), 0, slaughterHouse, product, cuts);
+    }
+}
