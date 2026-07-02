c78d172713827572822853eea37ea8f08039425b
2006288 <anthonyescuela@gmail.com>
2026-07-01 19:44:09 -0500

    hw26 V1.0

---
+++ hw/aimacana/u3/hw20AnimalFarmSystem/hw20Interfaces/src/main/java/ec/edu/espe/animalfarmsystem/model/FarmAnimal.java
@@ -0,0 +1,142 @@
+
+package ec.edu.espe.animalfarmsystem.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author LABS-ESPE
+ */
+public abstract class FarmAnimal {
+    
+    private int id;
+    private String breed;
+    private Date bornOn;
+    private float weight;
+    protected SlaughterHouse slaughterHouse;
+    private Product product;
+    private ArrayList<Cut> cut;
+
+    public FarmAnimal(int id, String breed, Date bornOn, float weight, SlaughterHouse slaughterHouse, Product product, ArrayList<Cut> cut) {
+        this.id = id;
+        this.breed = breed;
+        this.bornOn = bornOn;
+        this.weight = weight;
+        this.slaughterHouse = slaughterHouse;
+        this.product = product;
+        this.cut = cut;
+    }
+public int getAgeinMonths (){
+    if (bornOn==null){
+    throw new IllegalStateException("bornOnDate is not set");
+    
+    }
+    
+return 1;
+}
+
+public abstract void feed (Food food);
+    @Override
+    public String toString() {
+        return "FarmAnimal{" + "id=" + id + ", breed=" + breed + ", bornOn=" + bornOn + ", weight=" + weight + ", slaughterHouse=" + slaughterHouse + ", product=" + product + ", cut=" + cut + '}';
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the breed
+     */
+    public String getBreed() {
+        return breed;
+    }
+
+    /**
+     * @param breed the breed to set
+     */
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    /**
+     * @return the bornOn
+     */
+    public Date getBornOn() {
+        return bornOn;
+    }
+
+    /**
+     * @param bornOn the bornOn to set
+     */
+    public void setBornOn(Date bornOn) {
+        this.bornOn = bornOn;
+    }
+
+    /**
+     * @return the weight
+     */
+    public float getWeight() {
+        return weight;
+    }
+
+    /**
+     * @param weight the weight to set
+     */
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    /**
+     * @return the slaughterHouse
+     */
+    public SlaughterHouse getSlaughterHouse() {
+        return slaughterHouse;
+    }
+
+    /**
+     * @param slaughterHouse the slaughterHouse to set
+     */
+    public void setSlaughterHouse(SlaughterHouse slaughterHouse) {
+        this.slaughterHouse = slaughterHouse;
+    }
+
+    /**
+     * @return the product
+     */
+    public Product getProduct() {
+        return product;
+    }
+
+    /**
+     * @param product the product to set
+     */
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    /**
+     * @return the cut
+     */
+    public ArrayList<Cut> getCut() {
+        return cut;
+    }
+
+    /**
+     * @param cut the cut to set
+     */
+    public void setCut(ArrayList<Cut> cut) {
+        this.cut = cut;
+    }   
+}
