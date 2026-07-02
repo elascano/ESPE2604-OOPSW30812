c2df3045a31cd06216aa142cb0b5d137962ed1b9
ESTEBAN ALEXANDER BASURTO FARIAS <eabasurto@espe.edu.ec>
2026-07-01 23:08:53 -0500

    upload ws

---
+++ ws/basurto/u3/ws25/FarmSystem/src/main/java/ec/edu/espe/farmsystem/FarmSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espe.farmsystem;
+
+/**
+ *
+ * @author LABS-ESPE
+ */
+public class FarmSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
