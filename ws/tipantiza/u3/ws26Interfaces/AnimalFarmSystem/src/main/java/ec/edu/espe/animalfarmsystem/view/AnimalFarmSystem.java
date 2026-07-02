51a3cb74738f216cf028e4112317f2a3624b6f4f
Alexander Tipantiza <latipantiza1@espe.edu.ec>
2026-07-02 00:21:03 -0500

    ws26 Interfaces v1.0

---
+++ ws/tipantiza/u3/ws26Interfaces/AnimalFarmSystem/src/main/java/ec/edu/espe/animalfarmsystem/view/AnimalFarmSystem.java
@@ -0,0 +1,22 @@
+
+package ec.edu.espe.animalfarmsystem.view;
+
+import ec.edu.espe.animalfarmsystem.model.Cow;
+import ec.edu.espe.animalfarmsystem.model.Pig;
+import ec.edu.espe.animalfarmsystem.model.SlaughterHouse;
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author Alexander Tipantiza, The Softwariors, @ESPE
+ */
+public class AnimalFarmSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        
+        Pig pig = new Pig (0, 0, breed, bornOn, 0, slaughterHouse, product, cuts);
+
+    }
+}
