a49d8f6f3fd63da667da0de38c541550ec0d6042
Cristian-Palomo <cdpalomo@espe.edu.ec>
2026-07-19 20:42:43 -0500

    TemplateMethodV1.0

---
+++ hw/palomo/u3/hw25TemplateMethod/TemplateMethod.py/ec_edu_espe/templatemethod/model/coffee.py
@@ -0,0 +1,28 @@
+"""
+@author Cristian Palomo, Error 404, @ESPE
+"""
+from ec_edu_espe.templatemethod.model.caffeine_beverage import CaffeineBeverage
+
+
+class Coffee(CaffeineBeverage):
+
+    def brew(self) -> None:
+        print("Dripping coffee through filter")
+
+    def add_condiments(self) -> None:
+        print("Adding sugar and milk")
+
+    def wants_condiments(self) -> bool:
+        answer = self._get_user_input(
+            "Would you like milk and sugar with your coffee (y/n)? "
+        )
+        return answer.lower().startswith("y")
+
+    def _get_user_input(self, prompt: str) -> str:
+        try:
+            answer = input(prompt)
+        except EOFError:
+            answer = None
+        if answer is None:
+            return "no"
+        return answer
---
+++ hw/palomo/u3/hw25TemplateMethod/TemplateMethod.py/ec_edu_espe/templatemethod/view/beverage_view.py
@@ -0,0 +1,6 @@
+"""
+@author Cristian Palomo, Error 404, @ESPE
+"""
+class BeverageView:
+    def print_message(self, message: str) -> None:
+        print(message)
