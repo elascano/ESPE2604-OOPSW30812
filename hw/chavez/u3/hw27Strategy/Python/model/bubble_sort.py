eec2ba4baf50ee6ef7e4d137e2a8e40719c14ad1
odachavez <oychavez@espe.edu.ec>
2026-07-21 22:12:49 -0500

    hw27Strategy V1.0

---
+++ hw/chavez/u3/hw27Strategy/Python/model/bubble_sort.py
@@ -0,0 +1,13 @@
+from model.sorting_strategy import SortingStrategy
+
+class BubbleSort(SortingStrategy):
+
+    def sort(self, data):
+        arr = data.copy()
+
+        for i in range(len(arr)):
+            for j in range(len(arr) - i - 1):
+                if arr[j] > arr[j + 1]:
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+        return arr
