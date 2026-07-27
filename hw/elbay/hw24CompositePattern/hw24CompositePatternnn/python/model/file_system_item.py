3b376ff7c923b1c1ced07af89b40efed72151364
Didier Robin Elbay Enriquez <zeus.didier2006@gmail.com>
2026-07-26 19:50:30 -0500

    Delete hw/elbay/hw24CompositePattern directory

--- hw/elbay/hw24CompositePattern/hw24CompositePatternnn/python/model/file_system_item.py
+++
@@ -1,7 +0,0 @@
-class FileSystemItem:
-    def __init__(self):
-        self.name = "unnamed"
-        self.size = 0
-
-    def describe(self):
-        print(f"{self.name} - {self.size}KB")
--- hw/elbay/hw24CompositePattern/hw24CompositePatternnn/python/view/client.py
+++
@@ -1,7 +0,0 @@
-class Client:
-    root_item = None
-
-    @staticmethod
-    def do_client_tasks():
-        if Client.root_item is not None:
-            Client.root_item.describe()
