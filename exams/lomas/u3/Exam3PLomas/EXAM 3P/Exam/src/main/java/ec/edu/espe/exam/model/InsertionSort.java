dbbe137ee440f976e02d750f4a35fcc7e9e0232b
Lomas Haro Christopher Josue <cjlomas@espe.edu.ec>
2026-07-27 11:10:39 -0500

    LomasC Exam3P

---
+++ exams/lomas/u3/Exam3PLomas/EXAM 3P/Exam/src/main/java/ec/edu/espe/exam/model/InsertionSort.java
@@ -0,0 +1,19 @@
+package ec.edu.espe.exam.model;
+
+public class InsertionSort implements SortingStrategy {
+    @Override
+    public int[] sort(int[] data) {
+        int[] arr = data.clone();
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+        return arr;
+    }
+}
